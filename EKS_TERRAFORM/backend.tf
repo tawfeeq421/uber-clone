14ac9829bea338e3cb20d2ffe1af250eb293a0a6
Tawfeeq Ahmed <139829927+tawfeeq421@users.noreply.github.com>
2024-02-07 20:04:20 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "tawfeeq421" # Replace with your actual S3 bucket name
+    bucket = "tawfeeq4216233" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
